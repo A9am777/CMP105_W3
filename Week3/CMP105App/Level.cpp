3c4dd91eb77f9dd1a64103de36842c33f163f3f1
1900017 <1900017@uad.ac.uk>
2020-01-30 09:15:19 +0000

    Added bouncing circle

--- Week3/CMP105App/Level.cpp
+++ Week3/CMP105App/Level.cpp
@@ -6,7 +6,12 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	input = in;
 
 	// initialise game objects
-
+	circle.setFillColor(sf::Color::White);
+	circle.setRadius(25);
+	circle.setPosition(hwnd->getSize().x / 2, hwnd->getSize().y / 2);
+	circle.setOrigin(25, 25);
+	player = circle;
+	velocityCircle = sf::Vector2f(150, 200);
 }
 
 Level::~Level()
@@ -23,14 +28,35 @@ void Level::handleInput(float dt)
 // Update game objects
 void Level::update(float dt)
 {
-	
+	if (circle.getPosition().x+circle.getRadius() > window->getSize().x)
+	{
+		circle.setPosition(window->getSize().x - circle.getRadius(), circle.getPosition().y);
+		velocityCircle.x = -velocityCircle.x;
+	}
+	else if (circle.getPosition().x < circle.getRadius())
+	{
+		circle.setPosition(circle.getRadius(), circle.getPosition().y);
+		velocityCircle.x = -velocityCircle.x;
+	}
+	if (circle.getPosition().y + circle.getRadius() > window->getSize().y)
+	{
+		circle.setPosition(circle.getPosition().x, window->getSize().y - circle.getRadius());
+		velocityCircle.y = -velocityCircle.y;
+	}
+	else if (circle.getPosition().y < circle.getRadius())
+	{
+		circle.setPosition(circle.getPosition().x, circle.getRadius());
+		velocityCircle.y = -velocityCircle.y;
+	}
+
+	circle.setPosition(circle.getPosition() + dt * velocityCircle);
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-
+	window->draw(circle);
 	endDraw();
 }
 
